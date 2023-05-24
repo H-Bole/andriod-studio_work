036ddb7629b9e69908ad302bc5b028d4fc3c430a
Xiaoyao23333 <102267145+Ptuer-swpu@users.noreply.github.com>
2023-05-25 00:41:09 +0800

    模块化实现活动2，等待实现其余活动

--- app/src/main/java/com/example/app_huangbowei/fragment/ActicityFragment.java
+++ app/src/main/java/com/example/app_huangbowei/fragment/ActicityFragment.java
@@ -11,6 +11,10 @@ import com.example.app_huangbowei.R;
 //    精彩活动
 public class ActicityFragment extends BaseFragment {
     private ImageView imageView;
+    private ImageView imageView2;
+    private ImageView imageView3;
+    private ImageView imageView4;
+    private ImageView imageView5;
 
     public int getLayoutResId() {
         return R.layout.fragment_acticity;
@@ -23,28 +27,45 @@ public class ActicityFragment extends BaseFragment {
             @Override
             public void onClick(View v) {
                 // 加载另一个布局
-                loadAnotherLayout();
+                View view = LayoutInflater.from(getContext()).inflate(R.layout.activity_1, null);
+                ViewGroup rootView = (ViewGroup) getView().getParent();
+                rootView.addView(view);
+
+                // 添加返回按钮的点击事件
+                Button backButton = view.findViewById(R.id.btn_activity1);
+                backButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        // 返回原来的布局
+                        ViewGroup rootView = (ViewGroup) getView().getParent();
+                        rootView.removeViewAt(rootView.getChildCount() - 1);
+                    }
+                });
             }
         });
-    }
-
-    private void loadAnotherLayout() {
-        // 加载另一个布局
-        View view = LayoutInflater.from(getContext()).inflate(R.layout.activity_1, null);
-        ViewGroup rootView = (ViewGroup) getView().getParent();
-        rootView.addView(view);
-
-        // 添加返回按钮的点击事件
-        Button backButton = view.findViewById(R.id.btn_activity1);
-        backButton.setOnClickListener(new View.OnClickListener() {
+        imageView2 = rootView.findViewById(R.id.activity2_img);
+        imageView2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // 返回原来的布局
+                // 加载另一个布局
+                View view = LayoutInflater.from(getContext()).inflate(R.layout.activity_2, null);
                 ViewGroup rootView = (ViewGroup) getView().getParent();
-                rootView.removeViewAt(rootView.getChildCount() - 1);
+                rootView.addView(view);
+
+                // 添加返回按钮的点击事件
+                Button backButton = view.findViewById(R.id.btn_activity2);
+                backButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        // 返回原来的布局
+                        ViewGroup rootView = (ViewGroup) getView().getParent();
+                        rootView.removeViewAt(rootView.getChildCount() - 1);
+                    }
+                });
             }
         });
     }
+
 
     public void initData() {
         // 加载数据
